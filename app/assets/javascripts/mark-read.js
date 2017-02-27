48a47f3b75482def3242601ae2b4c5a434ad900d
Michael Scherer <michael.c.scherer@gmail.com>
2017-02-27 15:17:42 -0700

    Prepare for deploy

--- app/assets/javascripts/mark-read.js
+++ app/assets/javascripts/mark-read.js
@@ -10,7 +10,8 @@ $(document).ready(function(){
       url: '/api/v1/links/' + linkId,
       method: 'PATCH',
       data: {read: true}
-    }).then( updateStatus.bind($(this)) );
+    })
+    .then( updateStatus.bind($(this)) )
   })
 });
 
@@ -19,4 +20,14 @@ function updateStatus(link) {
 
   $linkDiv.children('p.link-read').html(link.read.toString());
   $linkDiv.toggleClass('true')
+  createHotRead(link.url)
+}
+
+function createHotRead(url) {
+  var data = {url: url}
+  $.ajax({
+    url: 'https://serene-shore-26911.herokuapp.com/api/v1/hot_reads',
+    method: 'POST',
+    data: data
+  });
 }
