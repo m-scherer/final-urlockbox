f4ca7310e6b6e62d1e0dac7d3bf26e6bb4051721
Michael Scherer <michael.c.scherer@gmail.com>
2017-02-27 10:20:23 -0700

    Add sad path testing for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to :root
     else
+      flash[:danger] = 'Please ensure email and password match'
       redirect_to :login
     end
   end
