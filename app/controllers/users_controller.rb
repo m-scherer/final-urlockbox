06f9b8d9fd5c0e4eb07ba3ff20bd10a92ecd3c0d
Michael Scherer <michael.c.scherer@gmail.com>
2017-02-27 10:16:46 -0700

    Add sad path testing for user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,13 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-    if user.save
+    if user_params[:password] != user_params[:password_confirmation]
+      redirect_to :signup
+      flash[:danger] = "Please ensure passwords match"
+    elsif User.find_by(email: user.email)
+      redirect_to :signup
+      flash[:danger] = "Please select a unique email"
+    elsif user.save
       session[:user_id] = user.id
       redirect_to :root
     else
@@ -17,6 +23,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
+
 end
